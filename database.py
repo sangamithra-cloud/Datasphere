f4b1a33eff4c08c32f9e6a74e42af8ac7b0fe81f
R <sangamithra@uniqnex360.com>
2026-02-08 14:35:50 +0530

    updated

--- database.py
+++ database.py
@@ -1,17 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import NullPool, create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 import os
 from config import DATABASE_URL
-
+from sqlalchemy.pool import NullPool
 
 DATABASE_URL=os.environ.get("DATABASE_URL")
 
-engine=create_engine(DATABASE_URL)
-sessionLocal=sessionmaker(autocommit=False,autoflush=False,future=True,bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=NullPool, )
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 Base=declarative_base()
 
 def get_db():
-    db = sessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
